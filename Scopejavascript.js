7df66a0abd1412284669cd7187c61aaf0617ec34
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-08 16:47:58 -0800

    Update Scopejavascript.js

--- Scopejavascript.js
+++ Scopejavascript.js
@@ -12,3 +12,20 @@ console.log(logCitySkyline());
 
 // console.log(skyscraper); would generate RefernceError: not defined.
 // Because we're trying to access it from outside the block of code.
+
+// Creating 3 global variables 
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+// We create a function (which will have a block of code in it)
+const callMyNightSky = () => {
+  // Here we are pulling global variables inside of our function.
+  return `Night Sky: ${satellite}, ${stars}, and ${galaxy}`;
+}
+
+/*
+When we call this function, it will be able to use the three variables
+used inside of it, becuase they are global variables, and are available to all lines of code.
+*/
+console.log(callMyNightSky());
