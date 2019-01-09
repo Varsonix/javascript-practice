5cbb7725001325631caf9159c30ed0b2bb7c7752
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-08 17:18:54 -0800

    Update javascript-arrays.js

--- javascript-arrays.js
+++ javascript-arrays.js
@@ -1,3 +1,14 @@
 // First ever Array
 const hobbies = ['Coding', 'Guitar', 'Gaming'];
 console.log(hobbies);
+
+// Simple array creation with strings.
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+//listItem contains the 1st item in the famousSayings Array.
+const listItem = famousSayings[0];
+
+// logging listItem will log the first item of famousSayings.
+console.log(listItem);
+console.log(famousSayings[2]); // logs the 3rd item from the famousSayings array.
+console.log(famousSayings[3]); // Logs undefined because there is no item in the 4th slot.
