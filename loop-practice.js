a0a1208499119db53ba6f03a7b13a8d54049c099
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-09 02:46:55 -0800

    Update loop-practice.js

--- loop-practice.js
+++ loop-practice.js
@@ -18,3 +18,36 @@ const vacationSpots = ['Bali', 'Paris', 'Tulum'];
 for (let i = 0; i < vacationSpots.length; i++){
   console.log(`I would love to visit ${vacationSpots[i]}!`);
 }
+
+// Creating a for loop to handle multiple arrays (nested for loop)
+const bobsFollowers = ['Fred', 'Mike', 'Clarissa', 'Jean'];
+const tinasFollowers = ['Fred', 'Mike', 'Alphonso'];
+const mutualFollowers = [];
+
+// For each time that this code iterates through bobsFollowers, each instance, it will fully run through tinasFollowers[];
+for (let i = 0; i < bobsFollowers.length; i++){
+  for (let j = 0; j < tinasFollowers.length; j++){
+    // When it finds that two indexes match, it will push it into my 3rd varaiable array.
+    if (bobsFollowers[i] === tinasFollowers[j]){
+      mutualFollowers.push(tinasFollowers[j]);
+    }
+  }
+}
+
+// Testing the code, recieved 'Fred] and 'Mike' in the 3rd array.
+console.log(mutualFollowers);
+
+/* 
+Creating a while loop to endlessly pick one of the 4 indexes in the array cards[] and assign it to currentCard until 
+spade is assigned and the while loops sees that currentCard === 'spade' and stops the while loop. the console.log
+is there to show me the different kinds of outputs thanks to the randomiser in the while loop.
+*/
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+let currentCard;
+
+let counter = 0;
+while (currentCard !== 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
