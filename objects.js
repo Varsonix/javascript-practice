899bc7649c594a253544b7c131dcf5626d1df3db
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-10 03:14:59 -0800

    Update objects.js

--- objects.js
+++ objects.js
@@ -34,3 +34,17 @@ let propName =  'Active Mission';
 
 const isActive = spaceship['Active Mission'];
 console.log(spaceship['Active Mission']);
+
+/*
+Re-assigning properties of objects and deleting properties of an object.
+*/
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+spaceship['color'] = 'glorious gold';
+spaceship.numEngines = 8;
+delete spaceship['Secret Mission'];
