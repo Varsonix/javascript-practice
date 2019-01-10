63671761346bfad91398728823fad6276ea0800a
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-10 02:54:24 -0800

    Update objects.js

--- objects.js
+++ objects.js
@@ -3,3 +3,17 @@ let fasterShip = {
   'Fuel Type': 'Turbo Fuel',
   color: 'silver'
 }
+
+/*
+Assigning properites of an object to variables.
+*/
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+const crewCount = spaceship.numCrew;
+const planetArray = spaceship.flightPath;
