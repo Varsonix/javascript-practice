4b473127c7607f1168e0b232b078cb3f4b5b1b4e
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-10 04:02:51 -0800

    Update objects.js

--- objects.js
+++ objects.js
@@ -109,3 +109,61 @@ let capFave = spaceship.crew.captain['favorite foods'][0];
 let firstPassenger = spaceship.passengers[0];
 // I did this one to make sure I was doing it right and to understand the whole "array index object name" thing.
 console.log(spaceship.passengers[1].hobbies);
+
+/*
+Playing with functions to re-assign properties of objects.
+*/
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+function greenEnergy(obj){
+  obj['Fuel Type'] = 'avocado oil';
+}
+
+function remotelyDisable(obj){
+  obj.disabled = true;
+}
+
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+
+console.log(spaceship);
+
+/*
+Using for loops to iterate through a list of objects.
+*/
+
+let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+}
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`);
+}
