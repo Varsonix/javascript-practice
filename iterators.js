8f9947d38a1162ef0d562748e24ee8b3026f6a79
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-09 23:51:04 -0800

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -99,3 +99,47 @@ const longFavoriteWords = favoriteWords.filter(function(word){
   return word.length > 7;
 });
 
+
+/*
+This lesson goes over using .findIndex() to location an index matching a condition.
+*/
+
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+/* --- Ultra Consice Notation ---
+const foundAnimal = animals.findIndex(i => i === 'elephant');
+*/
+/* --- Arrow notation ---
+const foundAnimal = animals.findIndex(i => {
+  return i === 'elephant';
+});
+*/
+// This will return the index location of 'elephant'
+const foundAnimal = animals.findIndex(function(i){
+  return i === 'elephant';
+});
+
+const startsWithS = animals.findIndex(function(j){
+  // return j[0] === 's'; can be used as well.
+   return j.charAt(0) === 's';
+});
+
+console.log(startsWithS);
+
+/*
+I have a really hard time trying to understand and re-iterate the concept of .reduce()
+Supposedly, it takes two arguments, an accumulator, and a currentValue. 
+The accumulator starts at the 1st element in an array, and the currentValue starts as the 2nd element in an array.
+As .reduce() iterates through the array, 
+the return value of the callback function becomes the accumulator value for the next iteration, 
+currentValue takes on the value of the current element in the looping process.
+*/
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce(function(accumulator, currentValue) {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10);
+
+console.log(newSum);
