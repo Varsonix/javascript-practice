4487ff976098e68354acd9c7bda2611d6d2f1175
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-10 03:44:57 -0800

    Update objects.js

--- objects.js
+++ objects.js
@@ -66,3 +66,46 @@ let alienShip = {
 // Invoking methods objectName.method(args);
 alienShip.retreat();
 alienShip.takeOff();
+
+/*
+Dealing with nested objects. Lession had us assign an array of nested objects to spaceship.passengers.
+*/
+let spaceship = {
+  // How exactly is this working? are the index numbers considered the object names?
+  // As in: spaceship.passengers[0] is technically an object? like the actual 0 is the object "essentially passenger1 or something'?
+  // and name: is an actual property of [0] essentially? spaceship.passengers[0] is the object and spaceship.passengers[0].name 
+  // is the property? is that why you can just start off an empty array with {} brackets? because it's techically already an object?
+  passengers: [{name: 'Space Dog'}, 
+               {name: 'Mark',hobbies: 'fishing'}],
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    backup: {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+// assigning capFace the value of spaceship.crew.captain['favorite foods'][0] (first index of favorite foods property.
+let capFave = spaceship.crew.captain['favorite foods'][0];
+// Testing the call for the first index of spaceship.passengers
+let firstPassenger = spaceship.passengers[0];
+// I did this one to make sure I was doing it right and to understand the whole "array index object name" thing.
+console.log(spaceship.passengers[1].hobbies);
