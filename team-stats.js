1eb17e86e11d628bb2ef0ba2553bbbac8a2b9040
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-10 22:38:29 -0800

    Update team-stats.js

--- team-stats.js
+++ team-stats.js
@@ -48,5 +48,25 @@ const team = {
     };
     
     this.players.push(player);
+  },
+  
+  addGame: function(oponnentName, teamPoints, oTeamPoints) {
+    let game = {
+      opponnent: oponnentName,
+      teamPoints: teamPoints,
+      opponnentPoints: oTeamPoints
+    }
+    
+    this.games.push(game);
   }
+  
 };
+
+team.addPlayers('Steph', 'Curry', 28);
+team.addPlayers('Lisa', 'Leslie', 44);
+team.addPlayers('Bugs', 'Bunny', 76);
+team.addGame('Tornadoes', 35, 55);
+team.addGame('Potatoes', 33, 99);
+team.addGame('Newbloods', 99, 1);
+console.log(team.players);
+console.log(team.games);
